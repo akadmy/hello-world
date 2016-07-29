750fe3edd8aaed9a4fef422cd489b65ee6f80973
akadmy <admin@akadmy.com>
2016-07-29 17:26:48 +0530

    Sum added to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 class Hello{
    public static void main(String [] args){
      System.out.println("Hello World");
+     
+     int a = 100;
+     int b = 200;
+     int sum = a+b;
+     System.out.println(String.valueOf(sum);
    }
 }
